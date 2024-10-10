bfd6c1e08e0d5d762a74ae4396e5cd46ec2492d9
Denis Lebedev <3d.lebedzeu@gmail.com>
2024-10-10 09:28:52 +0300

    Make a choice of algorithms for users

--- internal/application/maze/maze.go
+++ internal/application/maze/maze.go
@@ -3,6 +3,7 @@ package maze
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"os"
 	"sync"
 	"time"
@@ -16,18 +17,58 @@ import (
 
 const pathDrawingDelay = 50 * time.Millisecond
 
+type pathFinder interface {
+	ShortestPath(maze domain.Maze) ([]domain.Coord, bool)
+}
+
+func inputToMazeData(in *presentation.Input) domain.MazeData {
+	return domain.NewMazeData(in.Height, in.Width, in.Start, in.End)
+}
+
+func generationAlgorithm(algo string) generator.Algorithm {
+	switch algo {
+	case "prim":
+		return generator.NewPrim()
+
+	case "backtrack":
+		return generator.NewBacktrack()
+
+	default:
+		slog.Error("unknown generation algorithm")
+	}
+
+	return nil
+}
+
+func pathFinderAlgorithm(algo string) pathFinder {
+	switch algo {
+	case "dijkstra":
+		return pathfinder.NewDijkstra()
+
+	case "a-star":
+		return pathfinder.NewAStar()
+
+	default:
+		slog.Error("unknown path finder algorithm")
+	}
+
+	return nil
+}
+
 func Start() error {
 	input, output := os.Stdin, os.Stdout
 	pres := presentation.New(input, output)
 
-	data, err := pres.ProcessInput()
+	inputData, err := pres.ProcessInput()
 	if err != nil {
 		return fmt.Errorf("process input: %w", err)
 	}
 
-	gen := generator.New(generator.NewPrim())
-	paint := painter.New(output, data.Height, data.Width)
-	pathFinder := pathfinder.NewDijkstra()
+	mazeData := inputToMazeData(inputData)
+
+	gen := generator.New(generationAlgorithm(inputData.GenAlgo))
+	paint := painter.New(output, mazeData.Height, mazeData.Width)
+	pathFinder := pathFinderAlgorithm(inputData.PathFindAlgo)
 	paintingChan := make(chan domain.PaintingData)
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -41,7 +82,7 @@ func Start() error {
 		paint.PaintGeneration(ctx, paintingChan)
 	}()
 
-	maze, err := gen.GenerateMaze(data, paintingChan)
+	maze, err := gen.GenerateMaze(mazeData, paintingChan)
 	if err != nil {
 		cancel()
 		return fmt.Errorf("generate maze: %w", err)
--- internal/presentation/presentation.go
+++ internal/presentation/presentation.go
@@ -10,7 +10,8 @@ import (
 	"github.com/es-debug/backend-academy-2024-go-template/internal/domain"
 )
 
-const greetingMessage = `Welcome to the Maze Generator! The program generates a maze and a path for the shortest passage.
+const (
+	greetingMessage = `Welcome to the Maze Generator! The program generates a maze and a path for the shortest passage.
 
 Before we start, please keep the following in mind:
  - To display the maze correctly, you must enter the dimensions so that the maze fits into the console
@@ -22,20 +23,48 @@ Enjoy the program!
 
 `
 
+	generationAlgorithms = "prim backtrack"
+	pathFinderAlgorithms = "dijkstra a-star"
+)
+
+type Input struct {
+	Height       int
+	Width        int
+	Start        domain.Coord
+	End          domain.Coord
+	GenAlgo      string
+	PathFindAlgo string
+}
+
+func NewInput(height, width int, start, end domain.Coord, genAlgo, pathFindAlgo string) *Input {
+	return &Input{
+		Height:       height,
+		Width:        width,
+		Start:        start,
+		End:          end,
+		GenAlgo:      genAlgo,
+		PathFindAlgo: pathFindAlgo,
+	}
+}
+
 type dimension struct {
 	height int
 	width  int
 }
 
 type Presentation struct {
-	in  io.Reader
-	out io.Writer
+	in            io.Reader
+	out           io.Writer
+	genAlgos      []string
+	pathFindAlgos []string
 }
 
 func New(in io.Reader, out io.Writer) *Presentation {
 	return &Presentation{
-		in:  in,
-		out: out,
+		in:            in,
+		out:           out,
+		genAlgos:      strings.Fields(generationAlgorithms),
+		pathFindAlgos: strings.Fields(pathFinderAlgorithms),
 	}
 }
 
@@ -194,25 +223,75 @@ CoordLoop:
 	return end, nil
 }
 
-func (p *Presentation) ProcessInput() (domain.MazeData, error) {
+func (p *Presentation) algorithm(scan *bufio.Scanner, algos []string) (string, error) {
+	for i, algo := range algos {
+		fmt.Fprintf(p.out, " %d. %s\n", i+1, algo)
+	}
+
+	rng, err := newRange(newRangePoint(1, true), newRangePoint(len(algos), true))
+	if err != nil {
+		return "", fmt.Errorf("create range: %w", err)
+	}
+
+	algo, err := p.getInt(scan, rng)
+	if err != nil {
+		return "", fmt.Errorf("read generation algorithm from input stream: %w", err)
+	}
+
+	return algos[algo-1], nil
+}
+
+func (p *Presentation) generationAlgorithm(scan *bufio.Scanner) (string, error) {
+	fmt.Fprintln(p.out, "Choose maze generation algorithm:")
+
+	algo, err := p.algorithm(scan, p.genAlgos)
+	if err != nil {
+		return "", fmt.Errorf("p.algorithm(scan, %v): %w", p.genAlgos, err)
+	}
+
+	return algo, nil
+}
+
+func (p *Presentation) pathFinderAlgorithm(scan *bufio.Scanner) (string, error) {
+	fmt.Fprintln(p.out, "Choose path finder generation algorithm:")
+
+	algo, err := p.algorithm(scan, p.pathFindAlgos)
+	if err != nil {
+		return "", fmt.Errorf("p.algorithm(scan, %v): %w", p.pathFindAlgos, err)
+	}
+
+	return algo, nil
+}
+
+func (p *Presentation) ProcessInput() (*Input, error) {
 	fmt.Fprint(p.out, greetingMessage)
 
 	scan := bufio.NewScanner(p.in)
 
 	dim, err := p.mazeDimension(scan)
 	if err != nil {
-		return domain.MazeData{}, fmt.Errorf("getting dimension: %w", err)
+		return nil, fmt.Errorf("getting dimension: %w", err)
 	}
 
 	start, err := p.startCoord(scan, dim)
 	if err != nil {
-		return domain.MazeData{}, fmt.Errorf("getting start coord: %w", err)
+		return nil, fmt.Errorf("getting start coord: %w", err)
 	}
 
 	end, err := p.endCoord(scan, dim, start)
 	if err != nil {
-		return domain.MazeData{}, fmt.Errorf("getting end coord: %w", err)
+		return nil, fmt.Errorf("getting end coord: %w", err)
 	}
 
-	return domain.NewMazeData(dim.height, dim.width, start, end), nil
+	genAlgo, err := p.generationAlgorithm(scan)
+	if err != nil {
+		return nil, fmt.Errorf("getting generation algorithm: %w", err)
+	}
+
+	pathFindAlgo, err := p.pathFinderAlgorithm(scan)
+	if err != nil {
+		return nil, fmt.Errorf("getting path finder algorithm: %w", err)
+	}
+
+	return NewInput(dim.height, dim.width, start, end, genAlgo, pathFindAlgo), nil
 }
